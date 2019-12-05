5a01649e6890d116b2fab1bd85ebb03c0d49c272
DominikKrypel <krypel.dominik@gmail.com>
2019-12-05 19:00:16 +0100

    Reformat code

--- sandbox/src/main/java/MyFirstProgram.java
+++ sandbox/src/main/java/MyFirstProgram.java
@@ -7,23 +7,25 @@ public class MyFirstProgram {
         hello("user");
         hello("Dominik");
 
-        double len = 5;
-        System.out.println("Powierzchnia kwadratu o boku " + len + "=" + area(len));
+        Square s = new Square();
+        s.l = 5;
+        System.out.println("Powierzchnia kwadratu o boku " + s.l + "=" + area(s));
 
-        double a = 5;
-        double b = 6;
-        System.out.println("Powierzchnia prostąkąta o bokach  " + a + " i " + b + " = " + area(a, b));
+        Rectangle r = new Rectangle();
+        r.a = 5;
+        r.b = 6;
+        System.out.println("Powierzchnia prostąkąta o bokach  " + r.a + " i " + r.b + " = " + area(r));
     }
 
     public static void hello(String somebody) {// To jest funkcja.
         System.out.println("Hello, " + somebody + "!");
     }
 
-    public static double area (double l) {
-        return  l * l;
+    public static double area (Square s) {
+        return  s.l * s.l;
     }
 
-    public static double area (double c, double d) {
-        return c * d;
+    public static double area (Rectangle r) {
+        return r.a * r.b;
     }
 }

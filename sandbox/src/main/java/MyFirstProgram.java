1642b534e43a3f153cc7719531429abe558b2910
Dominik <krypel.dominik@gmail.com>
2020-02-09 20:39:53 +0100

    Update gitignore and update tests

--- sandbox/src/main/java/MyFirstProgram.java
+++ sandbox/src/main/java/MyFirstProgram.java
@@ -3,13 +3,13 @@ public class MyFirstProgram {
 
 
     public static void main(String[] args) { //Nazwy klas zaczynamy zawsze wielką literą, a wszystko pozostałe, np. pakiety, funckje z małej
-        hello("world");// Najpierw uruchomiła się funkcja main, która zwróciła się du funkcji hello i przekazała łańcuch world
-        hello("user");
-        hello("Dominik");
+        hello("world");// Najpierw uruchomiła się funkcja main, która zwróciła się du funkcji hello i przekazała łańcuch "world"
+        hello("user"); // Tak jak powyżej, tylko została przekazay łańcuch "user"
+        hello("Dominik");// Tak jak powyżej, tylko została przekazay łańcuch "Dominik"
 
         Square s = new Square();
         s.l = 5;
-        System.out.println("Powierzchnia kwadratu o boku " + s.l + "=" + area(s));
+        System.out.println("Powierzchnia kwadratu o boku " + s.l + " = " + area(s));
 
         Rectangle r = new Rectangle();
         r.a = 5;
@@ -17,7 +17,7 @@ public class MyFirstProgram {
         System.out.println("Powierzchnia prostąkąta o bokach  " + r.a + " i " + r.b + " = " + area(r));
     }
 
-    public static void hello(String somebody) {// To jest funkcja.
+    public static void hello(String somebody) {                     // To jest funkcja
         System.out.println("Hello, " + somebody + "!");
     }
 
--- sandbox/src/main/java/Square.java
+++ sandbox/src/main/java/Square.java
@@ -1,3 +1,4 @@
 public class Square {
+
     public double l;
 }

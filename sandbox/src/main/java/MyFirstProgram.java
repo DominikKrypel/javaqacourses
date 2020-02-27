d7cd48459e432a61ab551fdc62473fbe98490fff
Dominik <krypel.dominik@gmail.com>
2020-02-27 22:20:32 +0100

    update methods

--- sandbox/src/main/java/MyFirstProgram.java
+++ sandbox/src/main/java/MyFirstProgram.java
@@ -8,21 +8,13 @@ public class MyFirstProgram {
         hello("Dominik");// Tak jak powyżej, tylko została przekazay łańcuch "Dominik"
 
         Square s = new Square(5);
-        System.out.println("Powierzchnia kwadratu o boku " + s.l + " = " + area(s));
+        System.out.println("Powierzchnia kwadratu o boku " + s.l + " = " + s.area());
 
         Rectangle r = new Rectangle(5, 5);
-        System.out.println("Powierzchnia prostąkąta o bokach  " + r.a + " i " + r.b + " = " + area(r));
+        System.out.println("Powierzchnia prostąkąta o bokach  " + r.a + " i " + r.b + " = " + r.area());
     }
 
     public static void hello(String somebody) {                     // To jest funkcja
         System.out.println("Hello, " + somebody + "!");
-    }
-
-    public static double area (Square s) {
-        return  s.l * s.l;
-    }
-
-    public static double area (Rectangle r) {
-        return r.a * r.b;
     }
 }

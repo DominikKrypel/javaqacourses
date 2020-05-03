a8aa076141b0ec08448ad5e35647e9a7cc1d8cec
DominikKrypel <krypel.dominik@gmail.com>
2020-05-03 17:10:23 +0200

    Reformat code

---
+++ sandbox/src/test/java/pl/stqa/pft/sandbox/SquareTests.java
@@ -0,0 +1,10 @@
+package pl.stqa.pft.sandbox;
+
+public class SquareTests {                      /* W klasie, ktora zawiera test nie trzeba tworzyc metody main,
+                                                 poniewaz uruchamia sie framework testowy, to on znajduje wszystkie testy
+                                                i je wykonuje i generuje sprawozdanie */
+
+public void testArea(){
+    
+}
+}

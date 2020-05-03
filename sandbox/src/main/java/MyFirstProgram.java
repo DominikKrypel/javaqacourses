87508f556d7aed8cce5fa2b649caf04a35535ddf
DominikKrypel <krypel.dominik@gmail.com>
2020-05-03 13:43:23 +0200

    updated descriptions

--- sandbox/src/main/java/MyFirstProgram.java
+++ sandbox/src/main/java/MyFirstProgram.java
@@ -1,19 +1,21 @@
-public class MyFirstProgram {
+public class MyFirstProgram { //Klasa to opis struktury tego samego typu obieków,
+                              // które nazywają się przedstawicielami tej klasy albo egzemplarzami tej klasy
 
     public static void main(String[] args) {
         //Nazwy klas zaczynamy zawsze wielką literą, a wszystko pozostałe, np. pakiety, funckje z małej
         hello("world");// Najpierw uruchomiła się funkcja main, która zwróciła się du funkcji hello i przekazała łańcuch "world"
         hello("user"); // Tak jak powyżej, tylko została przekazay łańcuch "user"
-        hello("Dominik");// Tak jak powyżej, tylko została przekazay łańcuch "Dominik"
+        hello("tester");// Tak jak powyżej, tylko została przekazay łańcuch "Dominik"
 
-        Square s = new Square(5);
+        Square s = new Square(5); // przenosimy wartosci parametrow, uzupelniane sa w konstruktorze
         System.out.println("Powierzchnia kwadratu o boku " + s.l + " = " + s.area());
 
         Rectangle r = new Rectangle(5, 5);
         System.out.println("Powierzchnia prostąkąta o bokach  " + r.a + " i " + r.b + " = " + r.area());
     }
 
-    public static void hello(String somebody) {                     // To jest funkcja
+    public static void hello(String somebody) {                     // To jest funkcja , w nawiasie
+                                                                    // (String somebody) to jest parametr albo argument funkcji
         System.out.println("Hello, " + somebody + "!");
     }
 }
--- sandbox/src/main/java/Square.java
+++ sandbox/src/main/java/Square.java
@@ -1,9 +1,10 @@
-public class Square {
+public class Square { //wewnątrz klasy Square niezbędnie trzeba opisać...
 
-    public double l;
+    public double l; // ... atrybuty, które posiada obiekt tego typu.
 
-    public Square(double l) {
-        this.l = l;
+    public Square(double l) {  //to jest konstruktor
+       this.l = l;    //this - to jest ten obiekt, ktory inicjalizuje sie w instruktorze
+                       //
     }
 
     public double area() {

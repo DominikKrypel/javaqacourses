7ef5bf6276e15ba8f01bd65d6a76b9f7d0e67380
Dominik <krypel.dominik@gmail.com>
2020-08-05 11:05:49 +0200

    UpdateFirstTestNGTests

--- sandbox/src/test/java/pl/stqa/pft/sandbox/SquareTests.java
+++ sandbox/src/test/java/pl/stqa/pft/sandbox/SquareTests.java
@@ -1,5 +1,6 @@
 package pl.stqa.pft.sandbox;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class SquareTests {
@@ -8,8 +9,9 @@ public class SquareTests {
                                             to on znajduje wszystkie testy
                                            i je wykonuje i generuje sprawozdanie */
     @Test
-    public void testArea()  {
-        Square s = new Square(5);
-        assert s.area() == 25;
+    public void testArea() {
+        Square s = new Square(5);           // w metodzie testowej nie ma żadnego zwracanego wyników i paramterów, a o wynikach swojej pracy zawiadamia testowy framework
+
+        Assert.assertEquals(s.area(), 25.0);
     }
 }

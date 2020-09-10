c778d50587ce3a47c2702197375940cff4da8cbb
Dominik <krypel.dominik@gmail.com>
2020-09-10 21:39:59 +0200

    Assert equals for create group tests

--- addressbook-web-tests/src/test/java/pl/stqa/pft/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/pl/stqa/pft/addressbook/model/GroupData.java
@@ -3,23 +3,44 @@ package pl.stqa.pft.addressbook.model;
 import java.util.Objects;
 
 public class GroupData {
-    private final String id;
+    private int id;
     private final String name;
     private final String header;
     private final String footer;
 
     public GroupData(String name, String header, String footer) {
-        this.id = null;
+        this.id = 0;
         this.name = name;
         this.header = header;
         this.footer = footer;
     }
-    public GroupData(String id, String name, String header, String footer) {
+    public GroupData(int id, String name, String header, String footer) {
         this.id = id;
         this.name = name;
         this.header = header;
         this.footer = footer;
     }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getHeader() {
+        return header;
+    }
+
+    public String getFooter() {
+        return footer;
+    }
+
 
     @Override
     public boolean equals(Object o) {
@@ -41,21 +62,5 @@ public class GroupData {
                 "id='" + id + '\'' +
                 ", name='" + name + '\'' +
                 '}';
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getHeader() {
-        return header;
-    }
-
-    public String getFooter() {
-        return footer;
     }
 }
